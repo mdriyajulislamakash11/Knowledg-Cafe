783cad889c29e43229b0ecd04624ef26e660e6d4
akash ahmed <hafejmohammadakash@gmail..com>
2024-10-22 11:32:59 +0600

    first commit

---
+++ src/components/header/Header.jsx
@@ -0,0 +1,10 @@
+
+const Header = () => {
+    return (
+        <div>
+            <h1 className='text-4xl  fon-bold bg-blue-400'>Knowledg Cafe</h1>
+        </div>
+    );
+};
+
+export default Header;
